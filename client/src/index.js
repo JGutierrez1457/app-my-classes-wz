73affad4d4e2474fa3b51b2c5d4a8244b8e89431
JGutierrez1457 <jgutierrezch@uni.pe>
2021-05-10 19:12:48 -0500

    Delete Middleware Redux

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import todoApp from './reducers'
 import thunk from 'redux-thunk'
 
-const store = createStore(todoApp,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(todoApp);
 ReactDOM.render(
     <Provider store={store}>
         <App />
